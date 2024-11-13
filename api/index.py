7fbe384391ea722667ad2520faf08aa049225fe0
Malte Ubl <malte.ubl@gmail.com>
2024-11-13 08:18:36 -0800

    upgrade

--- api/index.py
+++ api/index.py
@@ -1,12 +1,8 @@
 from fasthtml.common import *
-import uvicorn
 
-app = FastHTML()
-rt = app.route
+app,rt = fast_app()
 
-@rt("/")
-def get():
-  return Title("FastHTML"), H1("Hello World!")
+@rt('/')
+def get(): return Div(P('Hello World!'), hx_get="/change")
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", default=8000)))
+serve()
